c26f3f66194c963b78d993c31d0bab683c3ff834
AdamHKaplan <AdamHKaplan@gmail.com>
2014-12-20 17:56:46 -0800

    heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,11 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "You have signed up successfully."
+      redirect_to root_path
     else
       flash[:alert] = "Player could not be created."
+      render :new
     end
-    redirect_to root_path
   end
 
   private
